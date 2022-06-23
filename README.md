7e7fa95cb58088cdc639c941faae552298e04056
David <davidalatishe@gmail.com>
2022-06-23 10:59:41 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#JS_Task1
+This is a javascript code which prints name,
+height, and country using alert without frontend.
---
+++ index.js
@@ -0,0 +1,11 @@
+alert("Name: Alatishe David\nHeight: 160cm\nCountry: Nigeria")
+
+
+
+let Name = 'Alatishe David';
+let Height = '160cm';
+let Country = 'Nigeria';
+
+console.log(Name);
+console.log(Height);
+console.log(Country);
